42dc7d638e8847e56b31c1d024f8c564ef04843b
Almaz Kinziabulatov <auqolaq@gmail.com>
2017-01-24 01:35:25 +0300

    Scriptable object based data component for different monsters types.

---
+++ Assets/Scripts/MonsterData.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public abstract class MonsterData : ScriptableObject {
+
+    [Header("Health Settings"), SerializeField, Range(1, 100)]
+    public int health;
+
+    [Header("Speed Settings"), SerializeField, Range(1, 10)]
+    public float speed;
+
+    [SerializeField, Range(1, 10)]
+    public float acceleration;
+
+    [SerializeField, Range(1, 100)]
+    public float angularSpeed;
+
+    [Header("Attack Settings"), SerializeField, Range(1, 10)]
+    public int damage;
+
+    [SerializeField, Range(1, 2)]
+    public float attackDistance;
+
+    [Header("Other Settings"), SerializeField, Range(0, 1)]
+    public float chanceToSpawn;
+
+    [SerializeField, Range(1, 100)]
+    public int rewardForKilling;
+}
