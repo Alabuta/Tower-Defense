72e0c1f4da8fc7b9fc2603db36b3c1113c9f657b
Almaz Kinziabulatov <auqolaq@gmail.com>
2017-01-24 01:37:20 +0300

    Monsters system WIP.

---
+++ Assets/Scripts/MonstersSystem.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class MonstersSystem : MonoBehaviour, ISystem {
+
+    public GameObject monsters;
+    Vector3 respawnPosition, finishPosition;
+
+    GameObject weakMonster;
+
+    int count = 0;
+
+    void Start()
+    {
+        respawnPosition = GameObject.FindWithTag("Respawn").transform.position;
+        finishPosition = GameObject.FindWithTag("Finish").transform.position;
+
+        weakMonster = Resources.Load<GameObject>("Prefabs/MonsterWeak");
+    }
+
+    void FixedUpdate()
+    {
+        StartCoroutine(ProduceMonsters());
+
+        if (count > 10)
+            StopCoroutine(ProduceMonsters());
+    }
+
+    IEnumerator ProduceMonsters()
+    {
+        yield return new WaitForSeconds(5);
+
+        monsters = Instantiate<GameObject>(weakMonster, respawnPosition, Quaternion.identity);
+
+        var agent = monsters.AddComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.SetDestination(finishPosition);
+
+        ++count;
+    }
+
+    void ISystem.UpdateSystem()
+    {
+        throw new NotImplementedException();
+    }
+
+    void AddMonster()
+    {
+    }
+
+    void KillMonster()
+    {
+    }
+
+    void Reset()
+    {
+        //monsters = FindObjectsOfType<MonsterComponent>();
+    }
+}
