2e205e305455d82668d215ab660d6e6e2074efcc
Almaz Kinziabulatov <auqolaq@gmail.com>
2017-01-25 00:03:57 +0300

    Monsters' parameters storing 'ScriptableObject' class is added. And new routine-class is implemented.

---
+++ Assets/Scripts/Monster/MonsterComponent.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using NavMeshAgent = UnityEngine.AI.NavMeshAgent;
+
+[ExecuteInEditMode, RequireComponent(typeof(NavMeshAgent))]
+public class MonsterComponent : MonoBehaviour {
+
+    [SerializeField]
+    public MonsterParams monsterParams;
+
+    void Reset()
+    {
+        var agent = gameObject.GetComponent<NavMeshAgent>();
+
+        if (agent == null)
+            agent = gameObject.AddComponent<NavMeshAgent>();
+    }
+
+    void OnValidate()
+    {
+#if UNITY_EDITOR
+        var agent = gameObject.GetComponent<NavMeshAgent>();
+
+        agent.radius = monsterParams.radius;
+
+        agent.speed = monsterParams.speed;
+        agent.angularSpeed = monsterParams.angularSpeed;
+        agent.acceleration = monsterParams.acceleration;
+        agent.stoppingDistance = 0.0f;
+        agent.autoBraking = false;
+
+        agent.avoidancePriority = monsterParams.avoidancePriority;
+#endif
+    }
+}
