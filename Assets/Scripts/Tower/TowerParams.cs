731d3e68c8fa0badadfadb169c51a32fc48c11d6
Almaz Kinziabulatov <auqolaq@gmail.com>
2017-01-28 00:04:00 +0300

    Tower's component and parameters scripts are implemented.

---
+++ Assets/Scripts/Tower/TowerParams.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewTowerParams", menuName = "New Type Of Tower", order = 2)]
+public class TowerParams : ScriptableObject {
+
+    [Header("Attack Settings"), Range(1, 100)]
+    public int hitDamage;
+
+    [Range(.1f, 10f)]
+    public float attackRadius;
+
+    [Range(.01f, 10f)]
+    public float fireRate;
+
+    [Range(.1f, 1.0f)]
+    public float projectileContactRadius;
+
+    [Header("Other Settings"), Range(1, 500)]
+    public int price;
+}
