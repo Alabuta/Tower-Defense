de899a02df5e55ad594f923d32ba66784226dbcd
Almaz Kinziabulatov <auqolaq@gmail.com>
2017-01-30 00:21:48 +0300

    Button prefab for towers' store chart..

---
+++ Assets/Scripts/UI/ButtonTooltip.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine.EventSystems;
+
+public sealed class ButtonTooltip : EventTrigger {
+
+    void Start()
+    {
+        HideToolTip();
+    }
+
+    public override void OnPointerEnter(PointerEventData data)
+    {
+        ShowToolTip();
+    }
+
+    public override void OnPointerExit(PointerEventData data)
+    {
+        HideToolTip();
+    }
+
+    public override void OnPointerClick(PointerEventData data)
+    {
+        HideToolTip();
+    }
+
+    void ShowToolTip()
+    {
+        transform.FindChild("TooltipPanel").gameObject.SetActive(true);
+    }
+
+    void HideToolTip()
+    {
+        transform.FindChild("TooltipPanel").gameObject.SetActive(false);
+    }
+}
